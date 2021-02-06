ed0213a6b2e7e0c7192c81edb33952db9df34928
ArcadeHustle <hostile@me.com>
2021-02-06 02:39:46 -0500

    cosmetic

--- MiSTer_update_games.sh
+++ MiSTer_update_games.sh
@@ -15,44 +15,49 @@ https://archive.org/details/MITS_Altair_8800_TOSEC_2012_04_23
 # https://archive.org/details/mister-dos-collection.7z
 
 # Apogee
+
 # Apple-II
+
 # Aquarius
 https://archive.org/details/Mattel_Aquarious_TOSEC_2012_04_23
 
 # Archie
+
 # A.SEGASYS1
+
 # Astrocade
 https://archive.org/details/Bally_Professional_Arcade_and_Astrocade_TOSEC_2012_04_23
 
 # Atari2600
-# https://archive.org/details/Atari2600FullRomCollectionReuploadByDataghost
+https://archive.org/details/Atari2600FullRomCollectionReuploadByDataghost
 
 # Atari5200
-# https://archive.org/details/Atari5200Collection
+https://archive.org/details/Atari5200Collection
 
 # Atari800
-# https://archive.org/details/Atari800FullRomCollectionReuploadByDataghost
+https://archive.org/details/Atari800FullRomCollectionReuploadByDataghost
 
 # AtariST
-# https://archive.org/details/AtariSTRomCollectionByGhostware
+https://archive.org/details/AtariSTRomCollectionByGhostware
 
 # AY-3-8500
 
 # BBCMicro
-# https://archive.org/details/AcornBBCMicroRomCollectionByGhostware
+https://archive.org/details/AcornBBCMicroRomCollectionByGhostware
 
 # BK0011M
+
 # C16
 https://archive.org/details/Commodore_C16_C116_Plus_4_TOSEC_2012_04_23
 
 # C64
-# https://archive.org/details/C64RomCollectionByGhostware
+https://archive.org/details/C64RomCollectionByGhostware
 
 # CO2650
 https://archive.org/details/Emerson_Arcadia_2001_TOSEC_2012_04_23
 
 # Coleco
-# https://archive.org/details/ColecoVisionCollection
+https://archive.org/details/ColecoVisionCollection
 
 # FXCAST
 
@@ -63,9 +68,11 @@ https://archive.org/details/Cyles_Gameboy_roms
 https://archive.org/details/GameBoyAdvanceTOSEC
 
 # Genesis
-# https://archive.org/details/SegaGenesisCollection
+https://archive.org/details/SegaGenesisCollection
 
 # hbmame
+Upate all pulls already
+
 # Jupiter
 https://archive.org/details/TOSEC_PIX_Jupiter_Cantab_2013-04-13
 https://archive.org/details/Jupiter_Cantab_Jupiter_Ace_TOSEC_2012_04_23
@@ -74,7 +81,9 @@ https://archive.org/details/Jupiter_Cantab_Jupiter_Ace_TOSEC_2012_04_23
 https://archive.org/details/VTech_Laser_310_TOSEC_2012_04_23
 
 # MacPlus
+
 # mame
+Upate all pulls already
 
 # MegaCD
 # https://archive.org/details/SegaCDCollection
@@ -90,11 +99,11 @@ https://archive.org/details/neo-geo-mvs-romset
 https://archive.org/details/neo-geo-aes-romset
 
 # NES
-# https://archive.org/details/famicom-disk-system-champion-collection
-# https://archive.org/details/NESrompack
+https://archive.org/details/famicom-disk-system-champion-collection
+https://archive.org/details/NESrompack
 
 # Odyssey2
-# https://archive.org/details/MagnavoxOdyssey2Collection
+https://archive.org/details/MagnavoxOdyssey2Collection
 
 # Oric
 https://archive.org/details/Tangerine_Oric_1_and_Atmos_TOSEC_2012_04_23
@@ -109,7 +118,7 @@ https://archive.org/details/Commodore_PET_TOSEC_2012_04_23
 https://archive.org/details/Sinclair_QL_TOSEC_2012_04_23
 
 # SamCoupe
-# https://archive.org/details/SamCoupeSpecialCollection
+https://archive.org/details/SamCoupeSpecialCollection
 
 # SharpMZ
 https://archive.org/details/Sharp_MZ-700_TOSEC_2012_04_23
@@ -121,7 +130,7 @@ https://archive.org/details/Sharp_MZ-800_and_MZ-1500_TOSEC_2012_04_23
 https://archive.org/details/SegaMasterSystemCollectionByGhostware
 
 # SNES
-# https://archive.org/details/SNESSpecialCollection
+https://archive.org/details/SNESSpecialCollection
 
 # Spectrum
 https://archive.org/details/ZXSpectrumTOSECSetV20171101LadyEklipse
@@ -130,7 +139,7 @@ https://archive.org/details/Sinclair_ZX_Spectrum_TOSEC_2012_04_23
 # SPMX
 
 # TGFX16
-# https://archive.org/details/TurboGrafx16AndPCEngineChampionCollection
+https://archive.org/details/TurboGrafx16AndPCEngineChampionCollection
 
 # TI-99_4A
 # TI99_4A
@@ -140,9 +149,11 @@ https://archive.org/details/basic-games-ti99
 https://archive.org/details/Tandy_TRS80_Color_Computer_TOSEC_2012_04_23
 
 # TSConf
+
 # Vector06
 
 # Vectrex
+
 # https://archive.org/details/VectrexCollection
 
 # VIC20
--- README.md
+++ README.md
@@ -1,4 +1,5 @@
-# MiSTer_update_games.sh
+# MiSTer_update_games.sh for use with MiSTerCade JAMMA adapter
+
 Download public ROM contents from archive.org into /media/fat/games for educational purposes 
 
 ### 
@@ -9,4 +10,7 @@ https://github.com/michaelshmitty/SD-Installer-macos_MiSTer
 Run update all after boot
 https://github.com/theypsilon/Update_All_MiSTer
 
+###
+Move configs over
+https://github.com/misteraddons/MiSTercade
 
