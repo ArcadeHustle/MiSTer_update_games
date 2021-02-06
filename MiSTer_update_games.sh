a1753bda01b0da5a027efef793cc5e295d4a1220
ArcadeHustle <hostile@me.com>
2021-02-06 03:00:36 -0500

    sort games

--- MiSTer_update_games.sh
+++ MiSTer_update_games.sh
@@ -2,167 +2,175 @@ MOUNT=`mount | grep mmcblk | cut -f3 -d " "`
 GAMES=`ls $MOUNT/games`
 echo $GAMES
 
-# AcornAtom
-https://archive.org/download/TOSEC_Main_Branch_Release_2018-12-27/Acorn/Atom/
+# Platforms I care about
 
-# Altair8800
-https://archive.org/details/MITS_Altair_8800_TOSEC_2012_04_23
-
-# Amiga
-https://archive.org/details/AmigaRomCollectionByGhostware_201711
-
-# ao486
-https://archive.org/details/mister-dos-collection.7z
-
-# Apogee
-
-# Apple-II
-
-# Aquarius
-https://archive.org/details/Mattel_Aquarious_TOSEC_2012_04_23
-
-# Archie
-
-# A.SEGASYS1
-
-# Astrocade
-https://archive.org/details/Bally_Professional_Arcade_and_Astrocade_TOSEC_2012_04_23
-
-# Atari2600
+## Atari2600
 https://archive.org/details/Atari2600FullRomCollectionReuploadByDataghost
 
-# Atari5200
+## Atari5200
 https://archive.org/details/Atari5200Collection
 
-# Atari800
+## Atari800
 https://archive.org/details/Atari800FullRomCollectionReuploadByDataghost
 
-# AtariST
+## AtariST
 https://archive.org/details/AtariSTRomCollectionByGhostware
 
-# AY-3-8500
-
-# BBCMicro
-https://archive.org/details/AcornBBCMicroRomCollectionByGhostware
-
-# BK0011M
-
-# C16
-https://archive.org/details/Commodore_C16_C116_Plus_4_TOSEC_2012_04_23
-
-# C64
+## C64
 https://archive.org/details/C64RomCollectionByGhostware
 
-# CO2650
-https://archive.org/details/Emerson_Arcadia_2001_TOSEC_2012_04_23
-
-# Coleco
+## Coleco
 https://archive.org/details/ColecoVisionCollection
 
-# FXCAST
-
-# GameBoy
+## GameBoy
 https://archive.org/details/Cyles_Gameboy_roms
 
-# GBA
+## GBA
 https://archive.org/details/GameBoyAdvanceTOSEC
 
-# Genesis
+## Genesis
 https://archive.org/details/SegaGenesisCollection
 
-# hbmame
-Upate all pulls already
-
-# Jupiter
-https://archive.org/details/TOSEC_PIX_Jupiter_Cantab_2013-04-13
-https://archive.org/details/Jupiter_Cantab_Jupiter_Ace_TOSEC_2012_04_23
-
-# Laser
-https://archive.org/details/VTech_Laser_310_TOSEC_2012_04_23
-
-# MacPlus
-
-# mame
-Upate all pulls already
-
-# MegaCD
+## MegaCD
 https://archive.org/details/SegaCDCollection
 
-# MSX
+## MSX
 https://archive.org/details/MSXRomCollectionByGhostware
 https://archive.org/details/MSX2RomCollectionByGhostware
 
-# MultiComp
-
-# NeoGeo
+## NeoGeo
 https://archive.org/details/neo-geo-mvs-romset
 https://archive.org/details/neo-geo-aes-romset
 
-# NES
+## NES
 https://archive.org/details/famicom-disk-system-champion-collection
 https://archive.org/details/NESrompack
 
-# Odyssey2
-https://archive.org/details/MagnavoxOdyssey2Collection
-
-# Oric
-https://archive.org/details/Tangerine_Oric_1_and_Atmos_TOSEC_2012_04_23
-
-# PDP1
-https://archive.org/download/TOSEC_Main_Branch_Release_2018-12-27/DEC/PDP-1/Games/
-
-# PET2001
-https://archive.org/details/Commodore_PET_TOSEC_2012_04_23
-
-# QL
-https://archive.org/details/Sinclair_QL_TOSEC_2012_04_23
-
-# SamCoupe
-https://archive.org/details/SamCoupeSpecialCollection
-
-# SharpMZ
-https://archive.org/details/Sharp_MZ-700_TOSEC_2012_04_23
-
-# SHARP MZ SERIES
-https://archive.org/details/Sharp_MZ-800_and_MZ-1500_TOSEC_2012_04_23
-
-# SMS
+## SMS
 https://archive.org/details/SegaMasterSystemCollectionByGhostware
 
-# SNES
+## SNES
 https://archive.org/details/SNESSpecialCollection
 
-# Spectrum
+## TGFX16
+https://archive.org/details/TurboGrafx16AndPCEngineChampionCollection
+
+
+# Platforms I do not care about (fight me!)
+
+## AcornAtom
+https://archive.org/download/TOSEC_Main_Branch_Release_2018-12-27/Acorn/Atom/
+
+## Altair8800
+https://archive.org/details/MITS_Altair_8800_TOSEC_2012_04_23
+
+## Amiga
+https://archive.org/details/AmigaRomCollectionByGhostware_201711
+
+## ao486
+https://archive.org/details/mister-dos-collection.7z
+
+## Apogee
+
+## Apple-II
+
+## Aquarius
+https://archive.org/details/Mattel_Aquarious_TOSEC_2012_04_23
+
+## Archie
+
+## A.SEGASYS1
+
+## Astrocade
+https://archive.org/details/Bally_Professional_Arcade_and_Astrocade_TOSEC_2012_04_23
+
+## AY-3-8500
+
+## BBCMicro
+https://archive.org/details/AcornBBCMicroRomCollectionByGhostware
+
+## BK0011M
+
+## C16
+https://archive.org/details/Commodore_C16_C116_Plus_4_TOSEC_2012_04_23
+
+## CO2650
+https://archive.org/details/Emerson_Arcadia_2001_TOSEC_2012_04_23
+
+## FXCAST
+Atari ST core
+
+## hbmame
+Upate all pulls already
+
+## Jupiter
+https://archive.org/details/TOSEC_PIX_Jupiter_Cantab_2013-04-13
+https://archive.org/details/Jupiter_Cantab_Jupiter_Ace_TOSEC_2012_04_23
+
+## Laser
+https://archive.org/details/VTech_Laser_310_TOSEC_2012_04_23
+
+## MacPlus
+
+## mame
+Upate all pulls already
+
+## MultiComp
+Z80 + 6502, 6809
+
+## Odyssey2
+https://archive.org/details/MagnavoxOdyssey2Collection
+
+## Oric
+https://archive.org/details/Tangerine_Oric_1_and_Atmos_TOSEC_2012_04_23
+
+## PDP1
+https://archive.org/download/TOSEC_Main_Branch_Release_2018-12-27/DEC/PDP-1/Games/
+
+## PET2001
+https://archive.org/details/Commodore_PET_TOSEC_2012_04_23
+
+## QL
+https://archive.org/details/Sinclair_QL_TOSEC_2012_04_23
+
+## SamCoupe
+https://archive.org/details/SamCoupeSpecialCollection
+
+## SharpMZ
+https://archive.org/details/Sharp_MZ-700_TOSEC_2012_04_23
+
+## SHARP MZ SERIES
+https://archive.org/details/Sharp_MZ-800_and_MZ-1500_TOSEC_2012_04_23
+
+## Spectrum
 https://archive.org/details/ZXSpectrumTOSECSetV20171101LadyEklipse
 https://archive.org/details/Sinclair_ZX_Spectrum_TOSEC_2012_04_23
 
-# SPMX
+## SPMX
 
-# TGFX16
-https://archive.org/details/TurboGrafx16AndPCEngineChampionCollection
-
-# TI-99_4A
-# TI99_4A
+## TI-99_4A
+## TI99_4A
 https://archive.org/details/basic-games-ti99
 
-# TRS-80
+## TRS-80
 https://archive.org/details/Tandy_TRS80_Color_Computer_TOSEC_2012_04_23
 
-# TSConf
+## TSConf
+ZX spectrum clone
 
-# Vector06
+## Vector06
 
-# Vectrex
- https://archive.org/details/VectrexCollection
+## Vectrex
+https://archive.org/details/VectrexCollection
 
-# VIC20
+## VIC20
 https://archive.org/details/CommodoreVIC20CollectionByGhostware
 https://archive.org/details/tosec-20161111-commodore-vic-20_20170305
 https://archive.org/details/Commodore_VIC20_TOSEC_2012_04_23
 
-# X68000
+## X68000
 https://archive.org/details/SharpX68000RomCollectionByGhostware
 
-# ZX81
+## ZX81
 https://archive.org/details/Sinclair_ZX81_TOSEC_2012_04_23
 
